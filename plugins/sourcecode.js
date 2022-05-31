25c0d80a5b82a2bdd2bf3e55a13e1473710e9008
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-06-01 06:29:41 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,9 +1,55 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'SCRIPT')).buffer(), `
-SCRIPT BOT
-`.trim(), 'https://github.com/davidpangrib001/One-V4\nJangan Lupa Kasih Stars 😁', 'Pemilik Bot', '#owner')
-handler.help = ['sourcecode']
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+	let fetch = require('node-fetch')
+    let fs = require('fs')
+const ftrol = {
+    key : {
+    remoteJid: 'status@broadcast',
+    participant : '0@s.whatsapp.net'
+    },
+    message: {
+    orderMessage: {
+    itemCount : 2022,
+    status: 1,
+    surface : 1,
+    message: 'Script FachriBotz By Fachri 🎨', 
+    orderTitle: `Menu ▸`,
+    thumbnail: await (await fetch(fla + 'Script')).buffer(), //Gambarnye
+    sellerJid: '0@s.whatsapp.net' 
+    }
+    }
+    }
+let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Source+code'
+let info = `◪ 📮 *SCRIPT BOT*
+│ *Script FachriBotz :* 
+│ ╰ 
+│ 
+│ *Base :*
+│ ╰ 
+│ *Base Ori :* 
+│ ╰ https://github.com/bochilgaming/games-wabot
+│ *RestApi :* https://hardianto.xyz
+╰─────═┅═────────
+📍 *Note :* 
+• Jangan lupa minta izin owner sebelum menggunakan scriptnya kak!
+• Jangan Lupa kasih star & kasih credit
+`.trim()
+await conn.send2Button(m.chat, info, `© ᴹᴿ᭄fαchríвσtzン`, 'Ok Fachri', 'ok', '💌 OWNER', '.owner', ftrol)
+}
+handler.help = ['sc']
 handler.tags = ['info']
-handler.command = /^(sourcecode|sc|scbot|script|github)$/i
+handler.command = /^(sc)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = false
 
 module.exports = handler
+
+let botol = global.botwm
