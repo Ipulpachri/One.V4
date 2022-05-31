beba1e0bb57372ac137c74db8ed2e546d2ee4251
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-06-01 06:35:11 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,14 +3,16 @@ let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fet
 *───────「 DONATE 」 ─────*
 ╭═══════════════
 ║╭─── [ DONASI ] ───
-║│➸ _*Pulsa*_✅: 
-║│- 0821 2847 5388
+║│➸ _*Pulsa*_: 
+║│- 628159917553
+║│➸ _*Dana*_:
+║│- 6285713041886
 ║│
 ║│➸ _*OWNER*_
 ║│- wa.me/6282160268332
 ║╰────────────
 ╰═══════════════
-`.trim(), 'Donasi jan asal mencet', 'Owner Bot', '.owner')
+`.trim(), 'Donasi Bg Biar Bot On 24Jam', 'Owner Bot', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
